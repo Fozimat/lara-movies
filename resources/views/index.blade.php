7a091a182e0c4ef43d00a2d085e2189609b69719
Fozimat <fozimata@gmail.com>
2021-10-13 15:18:46 +0700

    Styling

---
+++ resources/views/index.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.main')
+
+@section('content')
+<div class="container mx-auto px-4 pt-16">
+    <h2 class="uppercase tracking-wider text-orange-500 text-lg font-semibold">Popular Movies</h2>
+</div>
+@endsection
